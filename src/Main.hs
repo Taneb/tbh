c8d08301734b2cf4d4f8923096ec53c79d86e338
Nathan van Doorn <nvd1234@gmail.com>
2015-02-07 18:29:29 +0000

    Added folder serving

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import System.Directory
+import System.Environment
+import System.Exit
+import System.FilePath
+import System.IO
+
+import Web.Scotty
+
+fileServer :: FilePath -> ScottyM ()
+fileServer basePath = get (capture "/:filename") $ do
+  fileName <- fmap (basePath </>) $ param "filename"
+  file fileName
+  case takeExtension fileName of
+    ".html" -> setHeader "Content-Type" "text/html"
+    _ -> return ()
+
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> do
+      hPutStrLn stderr "Please supply a path to a directory"
+      exitFailure
+    (basePath:_) -> do
+      cond <- doesDirectoryExist basePath
+      if cond
+        then do
+        scotty 3000 $ fileServer basePath
+        else do
+        hPutStrLn stderr "That directory does not exist!"
+        exitFailure
